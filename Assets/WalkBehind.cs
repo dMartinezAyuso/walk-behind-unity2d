1b7a9d6927ea8c516e7618c0c0b98eb32c964acd
dMartinezAyuso <diego.martinez.ayuso@gmail.com>
2015-07-29 01:18:04 +0200

    test fixed

---
+++ Assets/WalkBehind.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WalkBehind : MonoBehaviour {
+
+	void OnTriggerEnter2D(Collider2D other) {
+		if (other.gameObject.tag == "Player") {
+			Debug.Log ("entra");
+			other.gameObject.GetComponent<SpriteRenderer>().sortingLayerName = "top";
+			this.GetComponent<SpriteRenderer>().sortingLayerName = "down";
+		}
+	}
+
+	void OnTriggerExit2D(Collider2D other) {
+		if (other.gameObject.tag == "Player") {
+			other.gameObject.GetComponent<SpriteRenderer>().sortingLayerName = "down";
+			this.GetComponent<SpriteRenderer>().sortingLayerName = "top";
+		}
+	}
+}
---
+++ Assets/inputs.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class inputs : MonoBehaviour {
+
+	float speed = 3;
+
+	void Update () {
+		Vector3 move = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
+		transform.position += move * speed * Time.deltaTime;
+	}
+}
